a862a3adb772cb01466caf5f1b9ba9b0da6a6a5c
Kunhee Ha <kunheeha@gmail.com>
2022-07-03 00:42:25 +0100

    luasnip supertab

--- .config/nvim/lua/kunheeha/cmp.lua
+++ .config/nvim/lua/kunheeha/cmp.lua
@@ -19,6 +19,20 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
+    ['<TAB>'] = cmp.mapping(function (fallback)
+      if luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif check_backspace() then
+        fallback()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
